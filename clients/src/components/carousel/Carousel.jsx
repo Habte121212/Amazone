4896ec98466295ed87ee34096fa8ba21889230c1
Habtamu <195909267+Habte121212@users.noreply.github.com>
2025-07-07 00:49:28 +0300

    carosal effect

---
+++ clients/src/components/carousel/Carousel.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Carousel as ResponsiveCarousel } from 'react-responsive-carousel'
+import { img } from '../../data'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import './carousel.scss'
+
+const AmazonCarousel = () => {
+  return (
+    <div>
+      <ResponsiveCarousel
+        autoPlay={true}
+        infiniteLoop={true}
+        showThumbs={false}
+        showIndicators={false}
+        showStatus={false}
+        stopOnHover={false}
+      >
+        {img.map((imageItem) => (
+          <div key={imageItem.id}>
+            <img src={imageItem.src} alt={imageItem.alt} />
+          </div>
+        ))}
+      </ResponsiveCarousel>
+      <div className="carouselBottom"></div>
+    </div>
+  )
+}
+
+export default AmazonCarousel
---
+++ clients/src/data.js
@@ -0,0 +1,13 @@
+import img1 from '/image1.png'
+import img2 from '/image2.png'
+import img3 from '/image3.png'
+import img4 from '/image4.png'
+
+export const image = [img3, img2, img1, img4]
+
+export const img = [
+  { id: 1, src: img1, alt: 'Image 1' },
+  { id: 2, src: img2, alt: 'Image 2' },
+  { id: 3, src: img3, alt: 'Image 3' },
+  { id: 4, src: img4, alt: 'Image 4' },
+]
