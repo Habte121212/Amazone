22235fd669c77ee00ca298ad6eeaa732a999cb25
Habtamu <195909267+Habte121212@users.noreply.github.com>
2025-07-14 14:28:06 +0300

    payments backend

---
+++ server/amazon-api/index.js
@@ -0,0 +1,39 @@
+
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+const stripe = require('stripe')(process.env.STRIPE_KEY);
+
+// initialize express
+const app = express();
+
+//middleware
+app.use(cors({ origin:true }))
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'success'})
+})
+
+
+// stripe payment
+app.post('/payment/create', async (req, res) => {
+  const total = req.query.total;
+  if(total > 0){
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total,
+      currency: 'usd'
+    })
+    res.status(201).json({ clientSecret: paymentIntent.client_secret });
+  }else{
+    res.status(400).json({ error: 'Total amount must be greater than zero'})
+  }
+})
+
+//port
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
