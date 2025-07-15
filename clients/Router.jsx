0ba463df765f0b28d462fc0c96a6791a41e04ef8
Habtamu <195909267+Habte121212@users.noreply.github.com>
2025-07-15 07:45:09 +0300

    payment page UI

--- clients/Router.jsx
+++ clients/Router.jsx
@@ -7,6 +7,12 @@ import Order from './src/components/pages/orders/Order'
 import Cart from './src/components/pages/cart/Cart'
 import ProductDetail from './src/components/pages/peoductDetail/ProductDetail'
 import Result from './src/components/pages/results/Result'
+import { Elements } from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
+
+const stripePromise = loadStripe(
+  'pk_test_51RkT1Z5Yz0LDOZ9LKRl6JAZemKZzgidDiIZMfWOyIaugu0aqilO7Hicp58Z4WBLKM4QXmkF6nvSX3M1RFPQ46SVX001aDZXmy5',
+)
 
 const Routing = () => {
   return (
@@ -15,7 +21,11 @@ const Routing = () => {
         <Route path='/' element={<Landing/>} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/order" element={<Order />} />
-        <Route path="/payment" element={<Payment />} />
+        <Route path="/payment" element={
+          <Elements stripe={stripePromise}>
+            <Payment />
+          </Elements>
+        } />
         <Route path="/product/:id" element={<ProductDetail />} />
         <Route path="/category/:categoryName" element={<Result />} />
         <Route path="/auth" element={<Signin />} />
--- clients/src/components/pages/payment/Payment.jsx
+++ clients/src/components/pages/payment/Payment.jsx
@@ -1,10 +1,92 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './payment.scss'
 import LayOut from '../../LayOut/LayOut'
+import { DataContext } from '../../DataProvider/DataProvider'
+import ProductCard from '../../products/ProductCard'
+import { useStripe, useElements, CardElement } from '@stripe/react-stripe-js'
+import { toast } from 'react-toastify'
+
 
 const Payment = () => {
+  const {
+    state: { user, cart },
+  } = useContext(DataContext)
+
+  // Total number of items (sum of quantities)
+  const totalItems = cart?.reduce((sum, item) => sum + (item.quantity || 1), 0)
+
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleChange = (e ) =>{
+    if (e.error) {
+      toast.error(e.error.message);
+    } else {
+      toast.success("Card details are valid");
+    }
+  }
+
   return (
-   <LayOut>payment</LayOut>
+    <LayOut>
+      <div className="paymentHeader">CheckOut {totalItems} items</div>
+      {/* payment method */}
+      <section className="payment">
+        {/* address */}
+        <div className="flex">
+          <h3>Delivery Address</h3>
+          <div>
+            {user ? (
+              <>
+                <div>{user.email}</div>
+                <div>{user.address?.line1 || 'No address on file'}</div>
+                <div>{user.address?.city || ''}</div>
+              </>
+            ) : (
+              <div>Please sign in to see your address</div>
+            )}
+          </div>
+        </div>
+        <hr />
+        {/* products */}
+        <div className="flex">
+          <h3>Reviw items and deliver</h3>
+        </div>
+        <div>
+          {cart.map((item) => (
+            <ProductCard product={item} key={item.id} flex={true} />
+          ))}
+        </div>
+        <hr />
+        {/* card forms */}
+        <div className="flex">
+          <h3>Payment Method</h3>
+          <div className="paymentMethod">
+            <div className='paymentDetail'>
+              <form action="">
+                <CardElement onChange={handleChange} />
+
+                {/* price */}
+                <div className='paymentPrice'> 
+                  <div>
+                    <p>Total Price: $</p>
+                      {cart
+                        .reduce(
+                          (sum, item) => sum + item.price * item.quantity,
+                          0,
+                        )
+                        .toFixed(2)}
+                    
+                  </div>
+                  <button>
+                    pay now
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </LayOut>
   )
 }
 
